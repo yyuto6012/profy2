5275b7732c1aa52ae6d841a716bc6a329360c246
yyuto6012 <(yyabu0618@gmail.com)>
2017-02-09 13:57:24 +0900

    create edit-function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,16 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def edit
+  end
+
+  def update
+    current_user.update(update_paramas)
+  end
+
+  private
+  def update_paramas
+    params.require(:user).permit(:family_name, :first_name,:family_name_kana,:first_name_kana )
+  end
 end
