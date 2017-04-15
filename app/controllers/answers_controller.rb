8c85223dcb1b2e90fccb4f4a1ac3241516aff207
yyuto6012 <(yyabu0618@gmail.com)>
2017-04-15 19:29:39 +0900

    create-1 q to 1 a function

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,4 +1,5 @@
 class AnswersController < ApplicationController
+  before_action :redirect_root, only: :new
   def new
     @question = Question.find(params[:question_id])
     @answer = @question.answers.new
@@ -12,5 +13,12 @@ class AnswersController < ApplicationController
   private
   def answer_params
     params.require(:answer).permit!
+  end
+
+  def redirect_root
+    @question = Question.find(params[:question_id])
+    if @question.answers.exists?(user_id: current_user.id)
+      redirect_to :root
+    end
   end
 end
