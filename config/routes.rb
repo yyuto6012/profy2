9c55ef986b4d182d5fae299e217f29f1c48cd398
yyuto6012 <(yyabu0618@gmail.com)>
2017-04-15 20:07:04 +0900

    show-answer-function

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show, :edit, :update]
   resources :groups, only: [:show, :edit, :update]
-  resources :questions, only: [:create]
+  resources :questions, only: [:create, :show]
   resources :answers, only: [:new, :create]
   root to: "top#index"
 end
