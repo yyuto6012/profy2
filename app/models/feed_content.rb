e99a6a7c97e48198426a50bfa40b8855b2b3bad8
yyuto6012 <(yyabu0618@gmail.com)>
2017-04-18 10:33:21 +0900

    create-feed_content model

---
+++ app/models/feed_content.rb
@@ -0,0 +1,2 @@
+class FeedContent < ApplicationRecord
+end
---
+++ db/migrate/20170418013254_create_feed_contents.rb
@@ -0,0 +1,8 @@
+class CreateFeedContents < ActiveRecord::Migration[5.0]
+  def change
+    create_table :feed_contents do |t|
+
+      t.timestamps
+    end
+  end
+end
