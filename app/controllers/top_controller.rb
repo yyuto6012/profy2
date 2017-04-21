9645f5cb224c6dbc7b3b6cbe03a723dceb656c75
yyuto6012 <(yyabu0618@gmail.com)>
2017-04-22 00:00:36 +0900

    set pagination

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -3,8 +3,8 @@ before_action :authenticate_user!, :only => [:show, :index]
 before_action :set_all_questions, :only => [:show, :index]
   def index
     @question = Question.new
-    feed_contents = current_user.group.feed_contents
-    @feed_contents = feed_contents.map(&:contentable)
+    @feed_contents = current_user.group.feed_contents.page(params[:page]).per(5)
+    @feed_contents_resource = @feed_contents.map(&:contentable)
   end
 
   def show
